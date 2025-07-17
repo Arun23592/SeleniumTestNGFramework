c72930efc3f4273c406372f6b630106d104a38da
arunsubramani <arunsubramani1992@gmail.com>
2025-07-17 22:19:05 +0530

    datepicker

--- src/test/java/automation/test/DatePicker.java
+++ src/test/java/automation/test/DatePicker.java
@@ -1,4 +1,34 @@
 package automation.test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class DatePicker {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void setUp() {
+        // Code to set up the test environment, e.g., initializing WebDriver, opening browser, etc.
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void DatePickerCalender(){
+    driver.findElement(By.cssSelector(".    react-date-picker__inputGroup")).click();
+    }
 }
