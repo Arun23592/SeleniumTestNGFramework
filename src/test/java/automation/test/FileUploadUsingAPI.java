3a1099c91591b47840fa7ab74666f2b56c54c73a
arunsubramani <arunsubramani1992@gmail.com>
2025-07-27 23:58:40 +0530

    file

--- src/test/java/automation/test/FileUploadUsingAPI.java
+++ src/test/java/automation/test/FileUploadUsingAPI.java
@@ -10,20 +10,28 @@ public class FileUploadUsingAPI {
 
     @Test
     public void uploadFile(){
+        RestAssured.useRelaxedHTTPSValidation();
+
         File zipfile = new File("FileUpload/DatabaseTestingAutomation.zip");
       Response response =  RestAssured
                 .given()
                     .multiPart("file", zipfile, "application/zip")   // form field 'file' or as per API spec
                     .contentType("multipart/form-data")
                 .when()
-                    .post("https://api.example.com/upload")
+                    .post("https://api.example.com/api/v1/upload")
                 .then()
                     .log().all()
-                    .assertThat()
                     .statusCode(200)
                     .extract().response();
 
-        System.out.println("Upload Responses: "+ response.toString());
+        System.out.println("Upload Responses: "+ response.asString());
 
     }
 }
+
+
+/*
+* When uploading ZIP files—or any file, really—multipart/form-data is essential because it’s designed to transmit binary data safely and efficiently over HTTP.
+*
+*
+* */
