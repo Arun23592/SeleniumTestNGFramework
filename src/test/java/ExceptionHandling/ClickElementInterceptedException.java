6893d77acb38b7b8d59be47235595aae4b8c3ea1
arunsubramani <arunsubramani1992@gmail.com>
2026-03-05 22:57:20 +0530

    clickelement

---
+++ src/test/java/ExceptionHandling/ClickElementInterceptedException.java
@@ -0,0 +1,12 @@
+package ExceptionHandling;
+
+import org.openqa.selenium.By;
+
+public class ClickElementInterceptedException {
+
+    public void safeClick(By locator){
+        try {
+
+        }
+    }
+}
