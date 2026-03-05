d2f5d9695b05b7dd5f55d1b7f6c883056c336a06
arunsubramani <arunsubramani1992@gmail.com>
2026-03-05 23:24:21 +0530

    clickelementex

--- src/test/java/ExceptionHandling/ClickElementInterceptedException.java
+++ src/test/java/ExceptionHandling/ClickElementInterceptedException.java
@@ -1,12 +1,30 @@
 package ExceptionHandling;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.*;
+
 
 public class ClickElementInterceptedException {
 
+    private WebDriver driver;
+    private JavascriptExecutor js;
+
+    public SafeActions(WebDriver driver){
+        this.driver = driver;
+        this.js = (JavascriptExecutor) driver;
+    }
+
     public void safeClick(By locator){
         try {
+            driver.findElement(locator).click();
+        }catch (ElementClickInterceptedException e){
+            System.out.println("Click interceoted - dismissing overlay and using JS click");
+
+
+            WebElement el = driver.findElement(locator);
 
         }
     }
+
+
+
 }
