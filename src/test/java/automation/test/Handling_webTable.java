a99d2818125a24bb6339ba412d179898a78df5ab
arunsubramani <arunsubramani1992@gmail.com>
2026-08-21 22:38:45 +0530

    webtable

--- src/test/java/automation/test/Handling_webTable.java
+++ src/test/java/automation/test/Handling_webTable.java
@@ -1,4 +1,28 @@
 package automation.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class Handling_webTable {
+
+    WebDriver driver;
+    public void setUp(){
+
+        driver = new ChromeDriver();
+        driver.get("https://www.tutorialspoint.com/selenium/practice/webtables.php");
+        driver.manage().window().maximize();
+
+    }
+
+
+    @Test
+    public void handleWebTable(){
+
+
+
+    }
 }
