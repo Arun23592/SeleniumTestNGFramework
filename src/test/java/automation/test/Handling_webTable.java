6c9421f1a9c6bccc72b69cc4a7e21b513e372a5f
arunsubramani <arunsubramani1992@gmail.com>
2026-08-23 22:32:38 +0530

    hand

--- src/test/java/automation/test/Handling_webTable.java
+++ src/test/java/automation/test/Handling_webTable.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -14,12 +15,13 @@ public class Handling_webTable {
 
     private static WebDriver driver;
     private static WebDriverWait wait;
+
+    @BeforeMethod
     public void setUp(){
 
         driver = new ChromeDriver();
         driver.get("https://www.tutorialspoint.com/selenium/practice/webtables.php");
         driver.manage().window().maximize();
-        Duration timeout;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
@@ -28,10 +30,16 @@ public class Handling_webTable {
     public void handleWebTable(){
 
         int total = 0;
-        List<WebElement> rows = driver.findElements(By.xpath("tbody tr:nth-child(1)"));
 
-        total += rows.size();
+        while(true){
+            List<WebElement> rows = driver.findElements(By.cssSelector("tbody tr"));
 
+            total += rows.size();
+
+            System.out.println("Total rows in the table: "+total);
+
+            break;
+        }
 
 
     }
