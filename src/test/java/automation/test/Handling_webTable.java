a2c7f9be9c805130357b6c9df32c188bf9e764c7
arunsubramani <arunsubramani1992@gmail.com>
2026-08-23 22:10:19 +0530

    handling

--- src/test/java/automation/test/Handling_webTable.java
+++ src/test/java/automation/test/Handling_webTable.java
@@ -1,11 +1,13 @@
 package automation.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 
 public class Handling_webTable {
@@ -17,13 +19,18 @@ public class Handling_webTable {
         driver = new ChromeDriver();
         driver.get("https://www.tutorialspoint.com/selenium/practice/webtables.php");
         driver.manage().window().maximize();
-
+        Duration timeout;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
 
     @Test
     public void handleWebTable(){
 
+        int total = 0;
+        List<WebElement> rows = driver.findElements(By.xpath("tbody tr:nth-child(1)"));
+
+        total += rows.size();
 
 
 
